662fe4ccf803731e8b9217d9834aca16a8df4695
fransjesky <fransjesky@hotmail.com>
2025-05-19 02:15:15 +0700

    added more languages support for formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,21 @@
 return {
   {
     "stevearc/conform.nvim",
-    dependencies = { "mason.nvim" },
-    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
     opts = {
       require("conform").setup({
         formatters_by_ft = {
           lua = { "stylua" },
           python = { "black" },
-          javascript = { "prettier" },
-          typescript = { "prettier" },
-          css = { "prettier" },
-          json = { "prettier" },
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+          javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+          typescript = { "prettierd", "prettier", stop_after_first = true },
+          typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+          graphql = { "prettierd", "prettier", stop_after_first = true },
+          json = { "prettierd", "prettier", stop_after_first = true },
+          markdown = { "prettierd", "prettier", stop_after_first = true },
+          html = { "prettierd", "prettier", stop_after_first = true },
+          css = { "prettierd", "prettier", stop_after_first = true },
         },
       }),
     },
