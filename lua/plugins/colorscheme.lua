7b281d074945c5fda651a7ac6a7f4df9488a1014
fransjesky <fransjesky@hotmail.com>
2025-05-22 02:39:58 +0700

    refactored some plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
@@ -7,6 +17,11 @@ return {
       require("catppuccin").setup({
         flavour = "mocha",
         transparent_background = true,
+        color_overrides = {
+          mocha = {
+            pink = "#89b4fa",
+          },
+        },
       })
     end,
   },
